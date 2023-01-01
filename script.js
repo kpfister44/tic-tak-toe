58eb082b916f6cfb368f5514b1c20ec44f72e152
Kyle Pfister <kpfister44@gmail.com>
2023-01-01 13:51:30 -0600

    fixed a few minor bugs such as the game saying it was a tie when player one won on the last turn. The program works as expected.

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const Player = (name) => {
    *                    game. Initially set to true.
    */
   const getName = () => name;
+  /**
+   * returns the name of the player
+   */
   let myTurn = true;
   return { getName, myTurn };
 };
@@ -22,11 +25,15 @@ const gameBoard = (() => {
   let totalTurns = 0;
 
   const hideDisplay = () => {
-    const pregameDisplay = document.querySelector(".pregame-display")
-    const gameDisplay = document.querySelector(".gameboard")
-    pregameDisplay.classList.toggle("inactive")
-    gameDisplay.classList.toggle("inactive")
-  }
+    /**
+     * This method takes no arguments and toggles the inactive class on and off for the preGameDisplay and
+     * the gameDisplay.
+     */
+    const pregameDisplay = document.querySelector(".pregame-display");
+    const gameDisplay = document.querySelector(".gameboard");
+    pregameDisplay.classList.toggle("inactive");
+    gameDisplay.classList.toggle("inactive");
+  };
   const displayBoard = () => {
     /**
      * this method takes no arguments and doesn't return anything. When called, it changes the
@@ -40,20 +47,27 @@ const gameBoard = (() => {
         gameboardTile.innerHTML = board[i];
       }
     }
-    const startGameButton = document.querySelector(".submit")
-    const playerForm = document.querySelector("form")
+    const startGameButton = document.querySelector(".submit");
+    const playerForm = document.querySelector("form");
     const playerTurnDisplay = document.querySelector(".player-turn-display");
+    // add an event listener that calls hideDisplay function when the Lets Play button is clicked
     startGameButton.addEventListener("click", hideDisplay);
-    playerForm.addEventListener("submit", (e)=> {
+    // add an event listener that call and annoymous function which stores the player 1 and player 2 names from
+    // the form and displays the names during the game
+    playerForm.addEventListener("submit", (e) => {
       e.preventDefault();
       const playersForm = e.target;
       displayNameOne = playersForm.playeronename.value;
       displayNameTwo = playersForm.playertwoname.value;
-      playerTurnDisplay.innerHTML = `${displayNameOne}'s Turn`
-      console.log(displayNameOne)
+      playerTurnDisplay.innerHTML = `${displayNameOne}'s Turn`;
     });
   };
   const checkEndGame = () => {
+    /**
+     * This method takes no arguments and checks to see if the end game state has been reached during the
+     * tic-tac-toe game.
+     */
+    // store the combination of winning conditions in a game of tic-tac-toe in an array
     const winningConditions = [
       board[0] + board[1] + board[2],
       board[0] + board[4] + board[8],
@@ -71,7 +85,7 @@ const gameBoard = (() => {
       } else if (i === "OOO") {
         endGame = true;
         winner = playerTwo;
-      } else if (totalTurns === 9) {
+      } else if (totalTurns === 9 && endGame == false) {
         endGame = true;
         winner = "tie";
       }
@@ -79,8 +93,16 @@ const gameBoard = (() => {
   };
 
   const playerTurn = () => {
+    /**
+     * This method takes no parameters. It is called when a user clicks on one of the tic-tac-toe tiles. It checks
+     * to see which player clicked the tile and updates the board array and calls the displayBoard method to reflect
+     * the board array change on the display. It also calls the checkEndGame method, and if the Endgame has been reached,
+     * displays the winner.
+     */
     const playerTurnDisplay = document.querySelector(".player-turn-display");
+    // update the totalTurns data attribute to keep track of how many turns have taken place
     totalTurns += 1;
+    // check to see if its player 1's turn
     if (playerOne.myTurn === true) {
       // update the board array index value to "x" based on the id of the tic-tac-toe tile the player clicked on
       board[targetTile.id] = playerOne.getName();
@@ -119,6 +141,7 @@ const gameBoard = (() => {
           once: true,
         });
       }
+      // check to see if the game ended in a tie
     } else if (endGame === true && winner === "tie") {
       playerTurnDisplay.innerHTML = "Tie Game";
       const ticTacToeSquares =
@@ -132,6 +155,10 @@ const gameBoard = (() => {
   };
 
   const restart = () => {
+    /**
+     * This method takes no arguments and resets all the data attributes to their original values. It also
+     * resets the display to its original display.
+     */
     board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     endGame = false;
     winner = null;
@@ -144,28 +171,29 @@ const gameBoard = (() => {
       gameboardTile.innerHTML = "";
     }
     playGame();
+    // change the player turn display so that the name of player 1 is shown
     const playerTurnDisplay = document.querySelector(".player-turn-display");
     playerTurnDisplay.innerHTML = `${displayNameOne}'s Turn`;
   };
 
   const playGame = () => {
+    /**
+     * This method takes no arguments and starts the tic-tac-toe game for the user. It calls
+     * the displayBoard method and adds eventlisteners to the tiles. It also adds the event 
+     * listener to the restart button.
+     */
     displayBoard();
     const ticTacToeSquares = document.getElementsByClassName("gameboard-tile");
     for (square of ticTacToeSquares) {
       square.addEventListener("click", (e) => {
         targetTile = e.target;
       });
-      square.addEventListener("click", gameBoard.playerTurn, { once: true });
+      square.addEventListener("click", playerTurn, { once: true });
     }
     const restartButton = document.querySelector(".restart-button");
     restartButton.addEventListener("click", restart, { once: true });
   };
   return {
-    board,
-    hideDisplay,
-    displayBoard,
-    playerTurn,
-    restart,
     playGame,
   };
 })();
